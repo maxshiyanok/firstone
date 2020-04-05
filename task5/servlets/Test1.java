c63340e975306e5a52b264cff93bcba059bc7417
maxshiyanok <60982310+maxshiyanok@users.noreply.github.com>
2020-04-05 20:28:29 +0300

    add servlets

---
+++ task5/servlets/Test1.java
@@ -0,0 +1,16 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Test1 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/status").forward(request, response);
+    }
+}
