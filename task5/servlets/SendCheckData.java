744d869b61cc3136cd0b0628696a46cc1a416122
maxshiyanok <60982310+maxshiyanok@users.noreply.github.com>
2020-04-08 13:47:12 +0200

    updated servlet

--- task5/servlets/SendCheckData.java
+++ task5/servlets/SendCheckData.java
@@ -3,14 +3,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-public class Check extends HttpServlet {
+public class SendCheckData extends HttpServlet {
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/no.json").forward(request, response);
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
