2140c95a01b81d47cc1c5be6c16f65951a03370f
maxshiyanok <60982310+maxshiyanok@users.noreply.github.com>
2020-04-08 13:51:54 +0200

    updated servlet

--- task5/servlets/StatusRequestForwarder.java
+++ task5/servlets/StatusRequestForwarder.java
@@ -1,15 +1,11 @@
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class Test1 extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
+public class StatusRequestForwarder extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/status").forward(request, response);
     }
