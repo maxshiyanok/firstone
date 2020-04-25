ee450d23b2b07931ba607d1be86fa7d16bca30b6
maxshiyanok <60982310+maxshiyanok@users.noreply.github.com>
2020-04-25 12:20:28 +0200

    Update class.js

--- task7/class.js
+++ task7/class.js
@@ -2,219 +2,227 @@ var posts = [
 	{
 		id: '1',
 		description: 'he',
-   	createdAt: new Date('2030-03-17T23:00:00'),
-   	author: 'Иванов Иван',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#helo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		createdAt: new Date('2030-03-17T23:00:00'),
+		author: 'Иванов Иван',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '2',
-   	description: 'Hi',
-   	createdAt: new Date('2000-03-17T23:00:00'),
-   	author: 'Боба шагун',
-   	photoLink: '',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'Hi',
+		createdAt: new Date('2000-03-17T23:00:00'),
+		author: 'Боба шагун',
+		photoLink: '',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '3',
-   	description: 'привет',
-   	createdAt: new Date('2030-03-17T23:50:00'),
-   	author: 'шаг бобов',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#geho','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'привет',
+		createdAt: new Date('2030-03-17T23:50:00'),
+		author: 'шаг бобов',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
   	},
 	{
 		id: '4',
-   	description: 'bye there',
-   	createdAt: new Date('2020-03-17T23:40:00'),
-   	author: 'Max Me',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'bye there',
+		createdAt: new Date('2020-03-17T23:40:00'),
+		author: 'Max Me',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '5',
-   	description: 'Hello there again',
-   	createdAt: new Date('2020-03-17T23:50:00'),
-   	author: 'Иванов Иван',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'Hello there again',
+		createdAt: new Date('2020-03-17T23:50:00'),
+		author: 'Иванов Иван',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '6',
-   	description: 'NI',
-   	createdAt: new Date('2020-03-18T02:00:00'),
-   	author: 'Not ME',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'NI',
+		createdAt: new Date('2020-03-18T02:00:00'),
+		author: 'Not ME',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '7',
-   	description: 'Hello there',
-   	createdAt: new Date('2020-03-18T03:00:00'),
-   	author: 'Pew Pew',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'Hello there',
+		createdAt: new Date('2020-03-18T03:00:00'),
+		author: 'Pew Pew',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '8',
-   	description: 'my POST',
-   	createdAt: new Date('2020-03-18T03:00:00'),
-   	author: 'Василий вася',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'my POST',
+		createdAt: new Date('2020-03-18T03:00:00'),
+		author: 'Василий вася',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '9',
-   	description: 'ПОСТЫ',
-   	createdAt: new Date('2020-03-18T06:00:00'),
-   	author: 'Иванов Иван',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'ПОСТЫ',
+		createdAt: new Date('2020-03-18T06:00:00'),
+		author: 'Иванов Иван',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '10',
-   	description: 'слыш',
-   	createdAt: new Date('2020-03-18T06:00:00'),
-   	author: 'Кабан',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'слыш',
+		createdAt: new Date('2020-03-18T06:00:00'),
+		author: 'Кабан',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '11',
-   	description: 'Hello there',
-   	createdAt: new Date('2020-03-18T23:00:00'),
-   	author: 'Иванов Иван',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'Hello there',
+		createdAt: new Date('2020-03-18T23:00:00'),
+		author: 'Иванов Иван',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '12',
-   	description: 'Hello there',
-   	createdAt: new Date('2020-03-19T23:00:00'),
-   	author: 'Иванов Иван',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'Hello there',
+		createdAt: new Date('2020-03-19T23:00:00'),
+		author: 'Иванов Иван',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '13',
-   	description: 'фЫврш гффы фы тв ргц фыв',
-   	createdAt: new Date('2000-03-20T23:00:00'),
-   	author: 'Да да я',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'фЫврш гффы фы тв ргц фыв',
+		createdAt: new Date('2000-03-20T23:00:00'),
+		author: 'Да да я',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '14',
-   	description: 'фЫврш гффы фы тв ргц фыв',
-   	createdAt: new Date('2020-03-20T23:00:00'),
-   	author: 'Да да я',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'фЫврш гффы фы тв ргц фыв',
+		createdAt: new Date('2020-03-20T23:00:00'),
+		author: 'Да да я',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '15',
-   	description: 'фЫврш гффы фы тв ргц фыв',
-   	createdAt: new Date('2020-03-21T23:00:00'),
-   	author: 'Макс Борис',
-   	photoLink: 'images/1.png',
+		description: 'фЫврш гффы фы тв ргц фыв',
+		createdAt: new Date('2020-03-21T23:00:00'),
+		author: 'Макс Борис',
+		photoLink: 'images/1.png',
 		hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '16',
-   	description: 'АХАХАХАХАХАХА',
-   	createdAt: new Date('2020-03-21T23:00:00'),
-   	author: 'Да да я',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'АХАХАХАХАХАХА',
+		createdAt: new Date('2020-03-21T23:00:00'),
+		author: 'Да да я',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '17',
-   	description: 'фЫврш гффы фы тв ргц фыв',
-   	createdAt: new Date('2020-03-22T23:00:00'),
-   	author: 'Да да я',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'фЫврш гффы фы тв ргц фыв',
+		createdAt: new Date('2020-03-22T23:00:00'),
+		author: 'Да да я',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '18',
-   	description: 'фЫврш гффы фы тв ргц фыв',
-   	createdAt: new Date('2020-03-23T23:00:00'),
-   	author: 'Да да я',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'фЫврш гффы фы тв ргц фыв',
+		createdAt: new Date('2020-03-23T23:00:00'),
+		author: 'Да да я',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '19',
-   	description: 'фЫврш гффы фы тв ргц фыв',
-   	createdAt: new Date('2020-03-24T23:00:00'),
-   	author: 'Да да я',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']
+		description: 'фЫврш гффы фы тв ргц фыв',
+		createdAt: new Date('2020-03-24T23:00:00'),
+		author: 'Да да я',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']
 	},
 	{
 		id: '20',
-   	description: 'фЫврш гффы фы тв ргц фыв',
-   	createdAt: new Date('2020-03-25T23:00:00'),
-   	author: 'Да да я',
-   	photoLink: 'images/1.png',
-   	hashTags: ['#gelo','#ni'],
-   	likes: ['max', 'Иванов Иван']	
+		description: 'фЫврш гффы фы тв ргц фыв',
+		createdAt: new Date('2020-03-25T23:00:00'),
+		author: 'Да да я',
+		photoLink: 'images/1.png',
+		hashTags: ['#gelo','#ni'],
+		likes: ['max', 'Иванов Иван']	
 	},
 ];
 class PostList{
 	constructor(posts){
 		this._posts = posts;
 	}
-	_comp(a, b){
+	_compareDates(a, b){
 		return a.createdAt - b.createdAt;
 	}
 	getPage(skip = 0, top = 10, filterConfig ={}) {
-			let new_posts = [];
+			let newPosts = [];
 			let filterKeys = Object.keys(filterConfig);
 			let filt;
 			if(filterKeys.length != 0)
 			{
 				filt = this._posts.filter(post => {
-					if(filterKeys[0] != 'hashTags')
-						return post[filterKeys[0]] == filterConfig[filterKeys[0]]
-					for(let tag of filterConfig.hashTags)
-						if(post.hashTags.indexOf(tag) >= 0)
-							return true;
-					return false;
+					for(let i = 0; i < filterKeys.length; i++){
+						let k = 0;
+						if(filterKeys[i] != 'hashTags')
+							{
+								if(post[filterKeys[i]] != filterConfig[filterKeys[i]])
+								return false;
+							}
+						else
+						{
+							for(let tag of filterConfig.hashTags)
+							{
+								if(post.hashTags.indexOf(tag) >= 0)
+									k++;
+							}
+							if(k == 0)
+							return false;
+						}
+				}
+				return true;
 				 });
 			}
 			else
 				filt = this._posts;
-			filt.sort(this.comp);
-			for (let i = skip; i < Math.min(skip + top, filt.length); i++) {
-				new_posts.push(filt[i]);
-			}
-			return new_posts;
-		}
+			filt.sort(this._compareDates);
+			filt = filt.slice(skip,Math.min(top, filt.length));
+			return filt;
+	}
 	get(id = '1'){
-		for(let post of this._posts)
-			if(post.id == id)
-				return post;
-		return null;
+		return this._posts.find(item => item.id == id);
 	}
 	static _validate(post){
 			if(!post.hasOwnProperty('id') || typeof(post.id)!='string')
@@ -237,14 +245,10 @@ class PostList{
 			return false;
 	}
 	edit(id,post){
-			let ind = 0;
-			for(let i = 0; i < this._posts.length;i++)
-				if(this._posts[i].id == id)
-					{
-						ind = i;
-						break;
-					}
-			let editPost = this._posts[ind];
+			let ind = this._posts.findIndex(item => item.id == id);
+			if(ind == -1)
+				return false;
+			let editPost = Object.assign({}, this._posts[ind]);
 			for(let key in post){
 				if(key != 'id' && key != 'createdAt' && key != 'author' && key!='likes')
 					editPost[key] = post[key];
@@ -255,33 +259,54 @@ class PostList{
 			return true;
 	}
 	remove(id){
-			for(let i = 0; i < this._posts.length; i++){
-				if(this._posts[i].id == id)
+			let ind = this._posts.findIndex(post => post.id == id);
+			if(ind != -1)
 				{
-					this._posts.splice(i,1);
+					this._posts.splice(ind,1)
 					return true;
 				}
-			}
 			return false;
 		}
 	addAll(posts){
 		let wrong = [];
-		for(let post of posts)
+		posts.forEach(post => {
 			if(PostList._validate(post))
 				this.add(post);
 			else
-				wrong.push(post);
+				wrong.push(post);})
 		return wrong;
 	}
 	clear(){
 		this._posts = [];
 		return true;
 	}
+	addOrRemoveLike(id, name){
+			let likes = this.get(id).likes;
+			let ind = likes.findIndex(like => like == name);
+			if(ind == -1)
+			{
+				likes.push(name);
+				return true;
+			}
+			else
+			{
+				likes.splice(ind,1);
+				return false;
+			}
+		}
 }
 
 let tweet = new PostList(posts);
+let invalid = {
+		id: '29',
+   	description: 'N2',
+   	createdAt: new Date('no date'),
+   	photoLink: 'images/1.png',
+   	hashTags: ['#gelo','#ni'],
+   	likes: ['max', 'Иванов Иван']
+	}
 console.log(posts);
-console.log(tweet.getPage(0,1,{author: 'Иванов Иван'}));
+console.log(tweet.getPage(0,5,{author: 'Иванов Иван'}));
 console.log(tweet.get('1'));
 console.log(tweet.get('100'));
 console.log(PostList._validate(posts[0]));
@@ -305,3 +330,5 @@ tweet.clear();
 console.log(tweet.getPage(0,10));
 tweet.addAll(posts);
 console.log(tweet.getPage(0,10,{hashTags: ['#gelo']}));
+console.log(tweet.addOrRemoveLike('1','maxim'));
+console.log(tweet.addOrRemoveLike('1','d'));
